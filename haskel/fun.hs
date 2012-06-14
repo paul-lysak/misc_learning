892050f14300f508d9a513d4bbae349900a27e75
paul-lysak <paul.lysak@gmail.com>
2012-06-14 15:48:46 +0300

    more haskel learing code

--- haskel/fun.hs
+++ haskel/fun.hs
@@ -1,3 +1,5 @@
+import Data.List
+
 add a b = a+b
 
 lastButOne a = head (cutToLength2 a)
@@ -31,5 +33,28 @@ mySum [] = 0
 myPalindrome (x:xs) = [x] ++ myPalindrome(xs) ++ [x]
 myPalindrome [] = []
 
-myIsPalindrome = 
+myIsPalindrome [] = True
+myIsPalindrome (x:[]) = True
+myIsPalindrome (x:xs) = (x == z)  && (myIsPalindrome middle)
+	where l = (length xs)
+              middle = (take (l-1) xs)
+              z = (head (drop (l-1) xs))
+
+sortBySublists superList = sortBy compareListLength superList
+	where compareListLength l1 l2 = compare (length l1) (length l2)
+
+joinSublists sep [] = [];
+joinSublists sep (subList:superList) = subList ++ sep ++ (joinSublists sep superList) 
+
+
+data Tree a = Node a (Tree a) (Tree a)
+	| Empty 
+	deriving (Show)
+
+sampleTree1 = (Node "root" (Node "left" Empty Empty) (Node "right" Empty Empty))
+sampleTree2 = (Node "root" (Node "left" Empty (Node "left-right" Empty Empty)) (Node "right" Empty Empty))
+
+treeDepth Empty = 0
+treeDepth (Node a left right) = 1 + max (treeDepth left) (treeDepth right)
+
 
