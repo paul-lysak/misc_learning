cc8acde642627011e827f21d2e2f056519aa9707
Paul Lysak <paul.lysak@gmail.com>
2012-09-04 18:39:45 +0300

    Polymorphic square calculation in Erlang

--- erlang/reclearn.erl
+++ erlang/reclearn.erl
@@ -1,7 +1,11 @@
 -module(reclearn).
--include("reclearn_def.hrl").
+% -include("reclearn_def.hrl").
+-record(circle, {radius}).
+-record(rectangle, {width, height}).
 -export([square/1]).
+%load records in shell: rr(reclearn).
 
 square(#circle{radius=R}) ->
-	3.14*R*R.
-
+	3.14*R*R;
+square(#rectangle{width=W, height=H}) ->
+	W*H.
